bf6bb1428a976324980f2652f44fe1d6c83680a4
Defier-learner <jiayangxu03@163.com>
2026-08-21 11:54:34 +0800

    修正了resUnet构建中的错误并改为pre-activation结构

--- archs_ResUnet.py
+++ archs_ResUnet.py
@@ -1,12 +1,18 @@
 '''
 ResUnet Architecture
+
 ResUnet: residual net is a net which learns the difference between the input and the output. 
 Unet: x->H(x)
 ResUnet: x->F(x)+x where F(x)=H(x)-x, thus F(x) represents the residual mapping to be learned.
 The maintaining of the identity mapping in ResUnet allows the network to learn more complex features.
 For example, the ibput image is composed of weak features and strong noises,
-the Unet will learn the strong noises and weak features together, while the ResUnet will learn the weak features and ignore the strong noises，
+the Unet will learn the strong noises and weak features together, while the ResUnet will learn the weak features and ignore the strong noises.
 which avoids the loss of weak features during the convolution process.
+Dynamic learning rate should be considered
+
+Special tips: pre-activation architecture is used in ResUnet, which means that the order of the convolutional layer and the activation function is reversed.
+This enables the maximum preservation of the original information in the input image, especially in the decoder process.
+Also, classical implementation of ResUnet should be considered.
 
 Similarly,the model should be imported to the register of archs before it can be used in train.py, val.py and test.py
 '''
@@ -18,11 +24,12 @@ class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv_block = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels)
         )
         if in_channels != out_channels:
             self.short_cut = nn.Sequential(
@@ -33,7 +40,7 @@ class ResidualBlock(nn.Module):
             self.short_cut = nn.Identity()
 
     def forward(self, x):
-        return nn.ReLU(inplace=True)(self.conv_block(x) + self.short_cut(x))
+        return self.conv_block(x) + self.short_cut(x)
 
 class DownSample(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -61,7 +68,16 @@ class ResUnet(nn.Module):
     def __init__(self, num_classes, input_channels=3,deep_supervision=False):
         super().__init__()
         nb_filter = [32, 64, 128, 256, 512]
-        self.inc = ResidualBlock(input_channels, nb_filter[0])
+        self.inc=nn.Sequential(
+            nn.Conv2d(input_channels, nb_filter[0], kernel_size=3, padding=1),
+            nn.BatchNorm2d(nb_filter[0]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(nb_filter[0], nb_filter[0], kernel_size=3, padding=1)
+        )
+        self.inc_short_cut=nn.Sequential(
+            nn.Conv2d(input_channels, nb_filter[0], kernel_size=1,bias=False),
+            nn.BatchNorm2d(nb_filter[0])
+        )
         self.down1 = DownSample(nb_filter[0], nb_filter[1])
         self.down2 = DownSample(nb_filter[1], nb_filter[2])
         self.down3 = DownSample(nb_filter[2], nb_filter[3])
@@ -73,7 +89,7 @@ class ResUnet(nn.Module):
         self.final = nn.Conv2d(nb_filter[0], num_classes, kernel_size=1)
 
     def forward(self, input):
-        x0_0 = self.inc(input)
+        x0_0 = self.inc(input)+self.inc_short_cut(input)
         x1_0 = self.down1(x0_0)
         x2_0 = self.down2(x1_0)
         x3_0 = self.down3(x2_0)
